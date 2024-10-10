6911a9d473f566af23a589e1cfc35b985e8260c5
abulhussian <abulkhanabulkhan106@gmail.com>
2024-10-10 14:49:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Skilled-e-learning-Tailwind-02
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+        colors: {
+          darkblue: "#13183F",
+          lightgrey: "#83869A",
+          pinkish: "#F74780",
+          linear: "#F0F1FF",
+          linear1: "#FF6F48",
+          linear2: "#F02AA6",
+          linear3: "#4851FF",
+        },
+    },
+  },
+  plugins: [],
+}
